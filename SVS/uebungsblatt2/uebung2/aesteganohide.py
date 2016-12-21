5b853a5f40cf7a7c123ecf8d3cec7138478ccb02
teagesea <stephanwag@gmx.net>
2016-12-21 13:02:20 +0100

    aufgabe fertig, kommentare werden nachgereicht

--- SVS/uebungsblatt2/uebung2/aesteganohide.py
+++ SVS/uebungsblatt2/uebung2/aesteganohide.py
@@ -1,25 +1,87 @@
 #!/usr/bin/python
 # -*- coding: utf8 -*-
 
-import steganohide, sys, hashlib, hmac, os
-from PIL import Image
+import steganohide
+import sys
+import hashlib
+import hmac
+import os
+import math
+import struct
 
+def xteaAlgorithm(key,block,n=32,endian="!"):
+    v0, v1 = struct.unpack(endian + "2L", block)
+    k = struct.unpack(endian + "4L", key)
+    sum, delta, mask = 0L, 0x9e3779b9L, 0xffffffffL
+    for round in range(n):
+        v0 = (v0 + (((v1 << 4 ^ v1 >> 5) + v1) ^ (sum + k[sum & 3]))) & mask
+        sum = (sum + delta) & mask
+        v1 = (v1 + (((v0 << 4 ^ v0 >> 5) + v0) ^ (sum + k[sum >> 11 & 3]))) & mask
+    return struct.pack(endian + "2L", v0, v1)
+
+
+def encryptCFB(key, data, iv='\00\00\00\00\00\00\00\00', n=32):
+    block = iv
+    result = ""
+    for i in range(int(math.ceil(len(data) / 8.0))):
+        plain = data[i * 8:i * 8 + 8]
+        eKey  = xteaAlgorithm(key, block, n)
+
+        xor = "".join([chr(ord(x)^ord(y)) for (x,y) in zip(plain, eKey)])
+        result = result + xor
+        block = xor
+
+    return result
+
+def decryptCFB(key, data, iv='\00\00\00\00\00\00\00\00', n=32):
+    block = iv
+    result = ""
+    for i in range(int(math.ceil(len(data) / 8.0))):
+        cipher = data[i * 8:i * 8 + 8]
+        eKey = xteaAlgorithm(key, block, n)
+
+        xor = "".join([chr(ord(x) ^ ord(y)) for (x, y) in zip(cipher, eKey)])
+        result = result + xor
+        block = cipher
+
+    return result
 
 def encryptMsg(mac, pw, txt):
 
     macSHA256 = hashlib.sha256(mac).digest()
     hashedMAC = hmac.new(macSHA256, txt, hashlib.sha256).digest()
-    return hashedMAC + txt
+    hData = hashedMAC + txt
+
+    hashedPW = hashlib.sha256(pw).digest()
+    key = hashedPW[:16]
+
+    encryptedData = encryptCFB(key, hData)
+
+    return encryptedData
+
+def xteaDecrypt(key, block, n=32, endian="!"):
+    v0, v1 = struct.unpack(endian+"2L", block)
+    k = struct.unpack(endian+"4L", key)
+    delta, mask = 0x9e3779b9L, 0xffffffffL
+    sum = (delta * n) & mask
+    for round in range(n):
+        v1 = (v1 - (((v0 << 4 ^ v0 >> 5) + v0) ^ (sum + k[sum >> 11 & 3]))) & mask
+        sum = (sum - delta) & mask
+        v0 = (v0 - (((v1 << 4 ^ v1 >> 5) + v1) ^ (sum + k[sum & 3]))) & mask
+    return struct.pack(endian+"2L", v0, v1)
 
 def decryptMsg(mac, pw, imgFilename):
-
     macSHA256 = hashlib.sha256(mac).digest()
+    hashedPW = hashlib.sha256(pw).digest()
+    key = hashedPW[:16]
 
     getCryptedText = steganohide.readTxtFromImage(imgFilename)
-    txt = getCryptedText[32:]
+    decryptedData = decryptCFB(key, getCryptedText)
+
+    txt = decryptedData[32:]
 
     hashedMAC = hmac.new(macSHA256, txt, hashlib.sha256).digest()
-    potentialMAC = getCryptedText[0:32]
+    potentialMAC = decryptedData[0:32]
 
     if potentialMAC == hashedMAC:
         return txt
@@ -42,9 +104,9 @@ if len(sys.argv) == 8 and mode == '-e':
     if steganohide.checkFileExistence(txtFilename, imgFilename):
         encryptedTxt = encryptMsg(mac, pw, steganohide.readTxtFile(txtFilename))
         if steganohide.writeTxtInImage(imgFilename, encryptedTxt):
-            sys.exit('Successfull')
+            sys.exit('Encoding Successfull')
         else:
-            sys.exit('Faild!')
+            sys.exit('Encoding Faild!')
 
 elif len(sys.argv) == 7 and mode == '-d':
     mac = sys.argv[3]
